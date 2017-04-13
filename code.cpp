b4ff5c987ab70ea5c29f815c501ad74601e1f769
FallahC <fallah@tuta.io>
2017-04-12 20:08:45 -0400

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -10,7 +10,7 @@ cout << "Please input your decimal number to convert it into binary. ";
 cin >> input;
 initial = input;
 c = 0;
-    while (input > 0){                /*This loop is to find the highest power of 2 that fits the input.*/
+    while (input > 0){                // This loop is to find the highest power of 2 that fits the input.
         if (input % 2 == 0){
             input = input / pow(2,1);
             answer_power = initial - pow(2,c);
@@ -21,21 +21,21 @@ c = 0;
         } 
             }
 
-    a = initial;                      /*a starts at the value that the user has chosen to convert.*/
-    b = pow(2,c);                     /*b starts at the highest power of 2 that fits a.*/
+    a = initial;                      // a starts at the value that the user has chosen to convert.
+    b = pow(2,c);                     // b starts at the highest power of 2 that fits a.
 
-    while (c >= 0){                   /*This loop is to do the actual conversion. It will input 1 when the next greatest power of 2 fits the remainder and 0 if it doesn't.*/
-        if (a - b >= 0){              /*Input 1 when the tested power of 2 fits the remainder.*/
+    while (c >= 0){                   // This loop is to do the actual conversion. It will input 1 when the next greatest power of 2 fits the remainder and 0 if it doesn't.
+        if (a - b >= 0){              // Input 1 when the tested power of 2 fits the remainder.
             cout << "1" << endl;
-            a = a - b;                /*a is set as the remainder value after suastracting with the current tested power of 2.*/
-            c = c - 1;                /*The power of 2 that will be tested next is reduced by 1.*/
-            b = pow(2,c);             /*b is set as the value of the reduced power of 2.*/
+            a = a - b;                // a is set as the remainder value after suastracting with the current tested power of 2.
+            c = c - 1;                // The power of 2 that will be tested next is reduced by 1.
+            b = pow(2,c);             // b is set as the value of the reduced power of 2.
             
         }
-        else if (a - b < 0){          /*Input 0 when the tested power of 2 is bigger than the remainder.*/
+        else if (a - b < 0){          // Input 0 when the tested power of 2 is bigger than the remainder.
             cout << "0" << endl;
-            c = c -1;                 /*The power of 2 that will be tested next is reduced by 1.*/
-            b = pow(2,c);             /*b is set as the value of the reduced power of 2.*/
+            c = c -1;                 // The power of 2 that will be tested next is reduced by 1.
+            b = pow(2,c);             // b is set as the value of the reduced power of 2.
         }
     }
     return 0;      
